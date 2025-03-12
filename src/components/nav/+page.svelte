5ba88646f6c6317dfc48410493391a1253691950
IzykGit <lancehemphill2000@gmail.com>
2025-03-12 06:53:41 -0400

    fixed responsiveness for nav, still needs dark mode

--- src/components/nav/+page.svelte
+++ src/components/nav/+page.svelte
@@ -8,7 +8,9 @@
 </script>
 
 <nav class="h-20 border-b">
-  <div class="max-w-7xl mx-auto flex items-center justify-between h-full">
+  <div
+    class="max-w-[1400px] px-12 mx-auto flex items-center justify-between h-full"
+  >
     <p class="text-xl font-bold">Where in the world?</p>
 
     <button class="cursor-pointer">
